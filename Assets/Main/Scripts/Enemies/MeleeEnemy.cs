db91491302087071a424186a5444599ff30d3a93
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-02-09 21:14:04 -0500

    added enemy explosion

--- Assets/Main/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Main/Scripts/Enemies/MeleeEnemy.cs
@@ -10,6 +10,8 @@ public class MeleeEnemy : EnemyBase
 {
     private Animator animator;
     private StatsBehaviour stats;
+
+    private bool primed;
     
     private void Start()
     {
@@ -28,16 +30,33 @@ public class MeleeEnemy : EnemyBase
         if (CurrentState == States.Moving)
         {
 
-            if (Vector3.Distance(PlayerObject.transform.position, transform.position) > 2f)
+            if (!primed && Vector3.Distance(PlayerObject.transform.position, transform.position) > 2f)
             {
                 animator.SetTrigger("Moving");
                 TargetPosition = PlayerObject.transform.position;
             }
-            else
+            else if (!primed)
             {
-                animator.SetTrigger("Idle");
+                animator.SetTrigger("Explode");
                 TargetPosition = transform.position;
+                StartCoroutine(Explode());
             }
         }
+    }
+
+    private IEnumerator Explode()
+    {
+        if (!primed)
+        {
+            primed = true;
+            yield return new WaitForSeconds(0.5f);
+            if (Vector3.Distance(PlayerObject.transform.position, transform.position) <= 2f)
+            {
+                PlayerObject.GetComponent<StatsBehaviour>().DamageHealth(stats.GetAttackDamage());
+                Debug.Log("damaged");
+            }
+            Destroy(this.gameObject);
+        }
+            
     }
 }
