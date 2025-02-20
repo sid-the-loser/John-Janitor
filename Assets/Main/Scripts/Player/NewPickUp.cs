ca0f0b423d380135eb18ab10b7bdf0a025fdec2a
GorayaU <144922343+GorayaU@users.noreply.github.com>
2025-02-20 16:59:13 -0500

    Revert "baana"
    This reverts commit ee63e30d724330aac36b60a9d2bd8b47ddb095a4.

--- Assets/Main/Scripts/Player/NewPickUp.cs
+++ Assets/Main/Scripts/Player/NewPickUp.cs
@@ -80,14 +80,6 @@ namespace Main.Scripts.Player
                         TypeThrowable = true; //insert false statement into the new drop funstion (YET TO BE CRETED)
                     }
                 }
-                else
-                {
-                    if (canDrop == true)
-                    {
-                        StopClipping(); //prevents object from clipping through walls
-                        DropObject();
-                    }
-                }
             }
             if (heldObj is not null) //if player is holding object
             {
@@ -98,15 +90,7 @@ namespace Main.Scripts.Player
                     StopClipping();
                     ThrowObject();
                 }
-            }
-            if (heldObjThrowable is not null)
-            {
-                MoveObject();
-                if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop == true) //Mous0 (leftclick) is used to throw, change this if you want another button to be used)
-                {
-                    StopClipping();
-                    ThrowObject();
-                }
+
             }
         }
         
@@ -213,22 +197,20 @@ namespace Main.Scripts.Player
             {
                 Weapons.ResetStats(1);
             }
+
             name = null;
             heldObj = null; //undefine game object
         }
-        
         void MoveObject()
         {
             if (TypeHeavy)
             {
                 //keep object position the same as the holdPosition position
                 heldObj.transform.position = holdPosHeavy.transform.position;
-            }
-            if (TypeWeapon)
+            }else if (TypeWeapon)
             {
                 heldObj.transform.position = holdPos.transform.position;
-            }
-            if (TypeThrowable)
+            }else if (TypeThrowable)
             {
                 heldObjThrowable.transform.position = holdPos.transform.position;
             }
@@ -276,7 +258,6 @@ namespace Main.Scripts.Player
                 heldObj.transform.parent = null;
                 heldObjRb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
                 heldObj = null;
-                TypeHeavy = false; TypeThrowable = false; //fix ltr
             }
         }
         
